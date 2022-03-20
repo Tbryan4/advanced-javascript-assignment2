d0d3bf06457a0f91a98b5effda973c878bae9adb
Tylen Bryan <tylenbryan@gmail.com>
2022-03-20 11:28:01 -0600

    code works up until this point

--- README.md
+++ README.md
@@ -4,4 +4,25 @@
 
 return a function that gives a programming link
 
-##
+## Adding The AuthStateChange Observer
+
+Inside the DashBoardPage function start by creating a piece of state called isUser.. The default state for a firebase user is false. Nobody should be allowed to access the page unless they are logged in. Next use the useNavigate method from the react router dom package. We will use this to return unauthorized users back to the login screen.
+Create the onAuthStateChange() to check to see if there is a current logged in user. If there is then we can change the state to true.
+
+
+  function DashBoardPage  (props){
+    const [isUser, setIsUser]= useState(false)
+    const navigator = useNavigate();
+
+    onAuthStateChanged(auth, (user) => {
+   
+        if (user) {
+            setIsUser(true)
+     
+        } else {
+            // error shouldn't get access return to the login page
+            setIsUser(false)
+            navigator('/')
+        }
+      });
+<br/>
--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import {onAuthStateChanged} from 'firebase/auth'
+import { auth } from 'libs/firebase';
+import { useNavigate } from 'react-router-dom';
+
 import { AppBar } from '../../components/appbar';
 import { Panel } from '../../components/Panel';
+
 
 
 // rfc for shortcut
 
 function DashBoardApp(props) {
-    return (
-       <>
-           <AppBar/>
-           
-           <Panel></Panel>
-           
-       </>
-    );
+
+    const [isUser, setIsUser] = useState(false)
+    const navigator = useNavigate();
+
+        onAuthStateChanged(auth, (user)=> {
+            if(user) {
+                setIsUser(true)
+            } else {
+                setIsUser(false)
+                //send user to login page
+                navigator("/")
+            }
+        })
+    if(isUser) { 
+        return (
+        <>
+            <AppBar/>     
+            <Panel></Panel>    
+        </>
+    )
+}else {
+        return null
+    };
 }
 
 export default DashBoardApp
