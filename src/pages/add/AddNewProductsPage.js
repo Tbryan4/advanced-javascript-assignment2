68d4d3834be630c69040478c0c88989c706029b3
Tylen Bryan <tylenbryan@gmail.com>
2022-03-25 12:44:03 -0600

    fixed error with add page route

--- src/pages/add/AddNewProductsPage.js
+++ src/pages/add/AddNewProductsPage.js
@@ -1,5 +1,9 @@
 
-import React from 'react'
+import React, {useState} from 'react'
+
+import {onAuthStateChanged} from 'firebase/auth'
+import { auth } from 'libs/firebase';
+import { useNavigate } from 'react-router-dom';
 
 import { AppBar } from '../../components/appbar';
 import {AddProductPanel} from 'components/Panel/AddProductPanel'
@@ -12,6 +16,21 @@ import {PannelBody } from 'components/Panel/styles'
 
 function AddNewProductsPage(props) {
     const pageName = "Add Products"
+
+    const [isUser, setIsUser] = useState(false)
+    const navigator = useNavigate();
+
+    onAuthStateChanged(auth, (user)=> {
+        if(user) {
+            setIsUser(true)
+        } else {
+            setIsUser(false)
+            //send user to login page
+            navigator("/")
+        }
+    })
+if(isUser) { 
+
     return (
         <>  
         <AppBar></AppBar>
@@ -21,6 +40,9 @@ function AddNewProductsPage(props) {
         </PannelBody>
         </>
     );
+}else {
+    return null
+};
 }
 
 export default AddNewProductsPage
