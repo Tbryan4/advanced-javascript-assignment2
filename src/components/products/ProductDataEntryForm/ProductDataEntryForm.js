7542f1e080de9894cac18e4eb2ae95f64da1bc92
Tylen <tbryan4@nait.ca>
2022-03-30 14:21:38 -0600

    updates

--- src/components/products/ProductDataEntryForm/ProductDataEntryForm.js
+++ src/components/products/ProductDataEntryForm/ProductDataEntryForm.js
@@ -1,25 +1,31 @@
 import React from 'react';
 
 import {ProductDataEntryFormStyles,ProductImage,ProductName,ProductPrice,ProductDescription} from './styles'
-import {Label} from 'ui/forms'
+import { ProductImageDropBox } from '../ProductImageDropBox';
+import {Label, Input} from 'ui/forms'
+import {TextArea} from 'ui/forms/textarea'
 
 function ProductDataEntryForm ({children, ...props})  {
   return (
         <ProductDataEntryFormStyles  {...props}>
            <ProductImage>
             <Label>Product Image</Label>
+            <ProductImageDropBox/>
            </ProductImage>
 
            <ProductName>
             <Label>Product Name</Label>
+            <Input/>
            </ProductName>
 
            <ProductPrice>
             <Label>Product Price</Label>
+            <Input/>
            </ProductPrice>
 
            <ProductDescription>
             <Label>Product ProductDescription</Label>
+            <TextArea/>
            </ProductDescription>
 
         </ProductDataEntryFormStyles>
--- src/components/products/ProductDataEntryForm/styles.js
+++ src/components/products/ProductDataEntryForm/styles.js
@@ -4,6 +4,18 @@ import styled from 'styled-components';
 
 const ProductDataEntryFormStyles  = styled.form`
       width: 380px;
+
+      input:focus, textarea:focus {
+            border-color: crimson;
+            background-color: #eff6ff;
+            outline-color: crimson;
+      }
+
+      fieldset {
+            display:flex;
+            gap: 1rem;
+            margin-bottom: 1.5rem;
+      }
 `;
 
 const ProductImage  = styled.div`
@@ -11,11 +23,11 @@ const ProductImage  = styled.div`
 `;
 
 const ProductName  = styled.div`
-      
+      flex: 3.5;
 `;
 
 const ProductPrice  = styled.div`
-      
+      flex:1.5;
 `;
 const ProductDescription  = styled.div`
       
